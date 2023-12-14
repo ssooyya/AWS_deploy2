0d0a2f9f8e76cd2640ecc76e980e2b500de211a1
goun3596 <goun3596@gmail.com>
2023-12-14 11:40:57 +0900

    Initial commit

---
+++ src/main/java/com/lec/spring/Controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.lec.spring.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HomeController {
+    @Value("${env.info}")
+    private String envInfo;
+
+    @GetMapping("/aws/v2")
+    public String hello(@RequestParam(defaultValue = "1")Integer num){
+        if(num == 1){
+            log.info("[" + envInfo +"] /aws/v1 info로그 #######");
+        } else if (num == -1) {
+            log.info("[" + envInfo +"] /aws/v1 error 로그 ##########");
+        } else if (num == 0) {
+            log.info("[" + envInfo +"] /aws/v1 warn 로그 ##########");
+        }
+        return "<h1>AWS 배포 v2</h1>";
+    }
+}
